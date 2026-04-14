3c8d21b1824c7db3525fa6660e1037b1b5641988
pedrinhodps <pehenmendes@gmail.com>
2026-04-13 22:56:20 -0300

    mais testes com db basica

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,9 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: bool | None = None
+    
+
+items_db = []
 
 
 @app.get("/")
@@ -16,10 +19,18 @@ def read_root():
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    item = items_db[item_id]  
+    return {"item_id": item_id, "name": item.name}
 
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
+def update_item(item_id: int):
+    item = items_db[item_id]
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    items_db.append(item)
+    return item
